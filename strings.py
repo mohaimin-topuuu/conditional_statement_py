dda20294ff3aa53e7777ee8fac15a91c286b6d1f
Mohaimin Topu <abmtopu73@gmail.com>
2025-01-17 11:55:44 +0600

    basics of string and it's operations

---
+++ strings.py
@@ -0,0 +1,65 @@
+#Concatenation in python
+
+# myFirstName = "Abdulla"
+# myMiddleName = "Al"
+# myLastName = "Mohaimin Topu"
+
+# myFullName = myFirstName +" " + myMiddleName +" " + myLastName
+
+# print(myFullName)
+
+#Indexing in python
+
+# myName = "Abdulla Al Mohaimin Topu"
+
+# indx = myName[3]
+
+# print(indx)
+
+#Slicing in python
+
+# myName = "Abdulla Al Mohaimin Topu "
+# myNameSlicing = myName[-5 : -1]
+# print(myNameSlicing)
+
+#Different string operations in python
+# myName = "Abdulla Al Mohaimin"
+# myStr = myName.endswith("in")
+# print(myStr)
+
+#capitalization in python
+
+# myName = "abdulla al mohaimin"
+
+# myNameCapitalized = myName.capitalize()
+
+# print(myNameCapitalized)
+
+#Replacing in python
+
+# myName = "Abdulla Al Mohaimin"
+
+# myNameReplaced = myName.replace("Abdulla Al", "Topu")
+
+# print(myNameReplaced)
+
+
+#Finding a word in a string in python
+
+
+# myName = "Mohaimin"
+
+# myNameFind = myName.find("a")
+
+# print(myNameFind)
+
+
+#Count in python
+
+# myStr = "John Doe"
+
+# myStrCount = myStr.count("o")
+
+# print(myStrCount)
+
+
